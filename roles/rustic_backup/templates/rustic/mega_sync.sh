240eb7fcae0bf25c04dc27a0ff84fdab72d0544e
Alejandro González <me@alegon.dev>
2025-02-04 19:48:31 +0100

    fix(rustic_backup): make mirroring to MEGA reliably work
    MegaCMD's WebDAV server has also proven to be troublesome when using
    rustic's OpenDAL integration, as it frequently returns spurious errors
    that may cause apparent or real repository corruption. I've tested a few
    other WebDAV clients, but all of them had pretty bad performance anyway,
    not fully utilizing the network bandwidth available by a long stretch,
    so the most likely conclusion is that such WebDAV server is lackluster.
    
    Therefore, as we can't use rclone's MEGA backend, and WebDAV has been
    also proven to be unusable, let's fall back to MegaCMD's folder
    synchronization feature. Luckily, this approach has been proven to be
    the most performant and stable by a wide margin, while straightforward
    enough to implement through rustic's hooks. The only downside is that
    such synchronization is bidirectional, so we should take care of not
    modifying the target MEGA folder in a way that corrupts the backup
    state.

---
+++ roles/rustic_backup/templates/rustic/mega_sync.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -eu
+
+readonly BACKUP_DIR='/backups/{{ item.identifier }}'
+readonly MEGA_FOLDER='{{ item.mega_folder }}'
+
+echo "$(date) Setting up synchronization of repository at $BACKUP_DIR to MEGA folder $MEGA_FOLDER"
+
+mega-login '{{ item.mega_user }}' '{{ item.mega_password }}' || true
+mega-sync --remove "$BACKUP_DIR" || true
+mega-sync "$BACKUP_DIR" "$MEGA_FOLDER" || true
+
+echo "$(date) Waiting for repository to be synchronized..."
+
+while
+  sync_status=$(mega-sync --enable --output-cols='STATUS' "$BACKUP_DIR")
+  sync_status=$(echo "$sync_status" | tail -1)
+  sleep 1
+  [ "$sync_status" != 'Synced' ]
+do
+  sleep 1
+done
+mega-sync --remove "$BACKUP_DIR"
+mega-quit || true
+
+echo "$(date) Synchronization complete"
